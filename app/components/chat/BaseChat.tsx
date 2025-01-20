a325ef1fd1dd360867ec4feacaa42db4fcaeacc2
persadian <34051185+arishma108@users.noreply.github.com>
2025-01-20 12:08:56 +0200

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -324,7 +324,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
                 <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
-                  Create New Solutions
+                  Create New Ideas
                 </h1>
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
                   Transform your concepts into reality or get help on active projects.
