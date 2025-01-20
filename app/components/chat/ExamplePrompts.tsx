b0fe2dab3d008e8154c3c93b2cd9f2863b07acfa
persadian <34051185+arishma108@users.noreply.github.com>
2025-01-20 11:19:08 +0200

    Update ExamplePrompts.tsx

--- app/components/chat/ExamplePrompts.tsx
+++ app/components/chat/ExamplePrompts.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const EXAMPLE_PROMPTS = [
-  { text: 'Build a todo app in React using Tailwind' },
-  { text: 'Build a simple blog using Astro' },
-  { text: 'Create a cookie consent form using Material UI' },
-  { text: 'Make a space invaders game' },
-  { text: 'Make a Tic Tac Toe game in html, css and js only' },
+  { text: 'Create a responsive AI-powered personal assistant' },
+  { text: 'Create a crowdfunding platform' },
+  { text: 'Create a clean, modern design of a directory website' },
+  { text: 'Create a SaaS dashboard' },
+  { text: 'Create an e-commerce site ' },
 ];
 
 export function ExamplePrompts(sendMessage?: { (event: React.UIEvent, messageInput?: string): void | undefined }) {
