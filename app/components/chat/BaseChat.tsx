2f45633b42b42ef6303200f601608c2546d0f3e8
persadian <34051185+arishma108@users.noreply.github.com>
2025-01-23 21:39:55 +0200

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -323,7 +323,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow lg:min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
               <div id="intro" className="mt-[16vh] max-w-chat mx-auto text-center px-4 lg:px-0">
-                 <img src="https://github.com/DARJYO/persadian-llm/blob/main/public/logo-light-styled.png" align="center" height="100%" width="100%">   
+                  <img src="https://github.com/DARJYO/persadian-llm/blob/main/public/logo-light-styled.png" height="100%" width="100%">  
                  <h1 className="text-3xl lg:text-6xl font-bold text-bolt-elements-textPrimary mb-4 animate-fade-in">
                   Create New Ideas
                 </h1>
